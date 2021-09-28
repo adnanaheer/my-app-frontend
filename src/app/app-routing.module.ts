3cca715d89b890168b1b6fa7bd9f3a33e7534233
Adnan Aheer <66543231+adnanaheer@users.noreply.github.com>
2021-09-28 16:16:58 +0500

    Added new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
   {
     path: 'invoice-builder',
     loadChildren: () => import('./invoice-builder/invoice-builder.module').then(m => m.InvoiceBuilderModule)
