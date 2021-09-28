a8305ee719db31f29d2eae737744d37c5c297108
Adnan Aheer <66543231+adnanaheer@users.noreply.github.com>
2021-09-28 16:29:16 +0500

    created side-nav and toolbar

--- src/app/invoice-builder/invoice-builder.module.ts
+++ src/app/invoice-builder/invoice-builder.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { InvoiceBuilderRoutingModule } from './invoice-builder-routing.module';
 import { InvoiceBuilderComponent } from './invoice-builder.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 
 @NgModule({
   declarations: [
     InvoiceBuilderComponent,
-    MainContentComponent
+    MainContentComponent,
+    SideNavComponent,
+    ToolbarComponent
   ],
   imports: [
     CommonModule,
